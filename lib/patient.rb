feb4f517aa28b439278ec034aaa31ea87af8eec4
Warda Ayaz <wayaz107@gmail.com>
2020-08-05 06:17:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,13 @@ def new_appointment(date,doctor)
   appointment = Appointment.new(date, self, doctor)
 end
 
+def appointments
+Appointment.all.select{|appt|appt.patient == self}
+end
 
+def doctors
+appointments.map {|appt|appt.doctor}
+end
 
 
 end
