24e79214ce68bf417131087265d2ad4e758b7a2e
Warda Ayaz <wayaz107@gmail.com>
2020-08-05 05:44:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,17 @@
 class Song
 
-end 
+@@all = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+end
